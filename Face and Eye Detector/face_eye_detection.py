1a8dd642653108cb0b0c037f3fd49f7d3f03b7fe
Vinay Sannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2018-07-24 21:40:33 +0200

    Update face_eye_detection.py

--- Face and Eye Detector/face_eye_detection.py
+++ Face and Eye Detector/face_eye_detection.py
@@ -16,3 +16,19 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # Lo
 eye_cascade  = cv2.CascadeClassifier('haarcascade_eye.xml') # Load the xml file for the eyes.
 
 #-----------------------------#
+
+#-----------------------------#
+########## Functions ##########
+#-----------------------------#
+
+"""Create a function that takes as input the image in black and white (gray_img) 
+and the original image (color_img), and that will return the same image with the 
+detector rectangles."""
+
+def detect(color_img, gray_img):
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.3, minNeighbors = 5) # detect faces in the image
+    for (x, y, w, h) in faces: # For each detected face:
+        cv2.rectangle(img = color_img, pt1 = (x,y),
+                      pt2 = (x+w, y+h), color = (255,100,100), thickness = 3) # draw a rectangle around the face
+    
+    return color_img
