fd0780fb5cf2f8bfb9aaea88ef1fb83b93b87614
Vinay Sannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2018-08-01 23:26:53 +0200

    Update Smile_detection.py

--- Smile Detector/Smile_detection.py
+++ Smile Detector/Smile_detection.py
@@ -1,1 +1,13 @@
 # Smile Detector using Open CV.
+"""
+Created on Sat Mar 10 21:26:19 2018
+
+@author: Vinay Sannaiah
+"""
+#Libraries
+import cv2 # import Open CV
+
+#Load the cascade files for Face(mandatory), Eyes(optional), Smile(mandatory)
+face_cascade  =  cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #Face
+eye_cascade   =  cv2.CascadeClassifier('haarcascade_eye.xml') #Eyes
+smile_cascade =  cv2.CascadeClassifier('haarcascade_smile.xml') #Smile
