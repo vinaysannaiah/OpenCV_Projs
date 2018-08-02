838a99a20fb663826d7959da708aa6e541180e43
Vinay Sannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2018-08-02 18:12:48 +0200

    Update Smile_detection.py

--- Smile Detector/Smile_detection.py
+++ Smile Detector/Smile_detection.py
@@ -11,3 +11,10 @@ import cv2 # import Open CV
 face_cascade  =  cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #Face
 eye_cascade   =  cv2.CascadeClassifier('haarcascade_eye.xml') #Eyes
 smile_cascade =  cv2.CascadeClassifier('haarcascade_smile.xml') #Smile
+
+#Functions
+
+def detect(color_img, gray_img):
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.3, minNeighbors = 5) # Detect the face first so that we can take this as a reference to detect the eyes and the mouth.
+    for (x, y, w, h) in faces:
+      
