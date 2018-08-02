2f1851d04fbcd871f061c85748a470a183e5e195
Vinay Sannaiah <36826364+vinaysannaiah@users.noreply.github.com>
2018-08-02 20:32:21 +0200

    Update face_eye_detection.py

--- Face and Eye Detector/face_eye_detection.py
+++ Face and Eye Detector/face_eye_detection.py
@@ -1,25 +1,14 @@
 # Face Recognition - creating a bounding box around the Face and also around the Eyes
 
-#-----------------------------#
 ########## Libraries ##########
-#-----------------------------#
-
 import cv2 # Import Open CV
 
-#-----------------------------#
 
-#-----------------------------#
 ######## Load the xmls ########
-#-----------------------------#
-
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # Load the xml for the face.
 eye_cascade  = cv2.CascadeClassifier('haarcascade_eye.xml') # Load the xml file for the eyes.
 
-#-----------------------------#
-
-#-----------------------------#
 ########## Functions ##########
-#-----------------------------#
 
 """Create a function that takes as input the image in black and white (gray_img) 
 and the original image (color_img), and that will return the same image with the 
